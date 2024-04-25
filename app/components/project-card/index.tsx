ff27c20f5a238d3aabe1fa5281279518f2e6ab9a
Bowornthat <bowornthat.maize@gmail.com>
2024-04-25 09:19:42 +0000

    adjust project-card

--- app/components/project-card/index.tsx
+++ app/components/project-card/index.tsx
@@ -8,6 +8,7 @@ import {
   CardTitle,
 } from "../ui/card";
 import Image from "next/image";
+import { GithubIcon } from "lucide-react";
 
 export interface ProjectCardProps {
   id: string;
@@ -21,24 +22,24 @@ export interface ProjectCardProps {
 
 export default function ProjectCard(props: ProjectCardProps) {
   return (
-    <Card>
+    <Card className="lg:max-w-md w-[384px] h-[150px]">
       <div>
         <Image
           src={
             "https://th.bing.com/th/id/R.6dadd6a39af8ca284e56354ee099752e?rik=JXGf1AD1SgQOPg&pid=ImgRaw&r=0"
           }
           alt=""
-          width={200}
-          height={200}
-          className="aspect-square rounded-md object-cover overflow-hidden"
+          width={384}
+          height={272}
+          className="aspect-square rounded-md object-cover overflow-hidden w-full md:w-auto max-h-fit"
         />
       </div>
       <CardHeader>
-        <CardTitle>Project Title</CardTitle>
-        <CardDescription>Project Description</CardDescription>
+        <CardTitle>Discord Bot</CardTitle>
+        <CardDescription>Python</CardDescription>
       </CardHeader>
-      <CardContent>Project Content</CardContent>
-      <CardFooter>
+      <CardContent>Programming Discord Bot for using in server to help doing simple tasks, improving thinking and coding skills.</CardContent>
+      <CardFooter className="flex justify-end">
         <a href="Project URL">View Project</a>
       </CardFooter>
     </Card>
