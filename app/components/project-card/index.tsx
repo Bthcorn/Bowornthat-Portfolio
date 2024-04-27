b40bf33bf75132d8058c098e5405c5e34922df06
Bowornthat <bowornthat.maize@gmail.com>
2024-04-27 08:07:18 +0000

    50% project-card complete

--- app/components/project-card/index.tsx
+++ app/components/project-card/index.tsx
@@ -8,7 +8,8 @@ import {
   CardTitle,
 } from "../ui/card";
 import Image from "next/image";
-import { GithubIcon } from "lucide-react";
+import { Badge } from "../ui/badge";
+import { Separator } from "../ui/separator";
 
 export interface ProjectCardProps {
   id: string;
@@ -22,25 +23,37 @@ export interface ProjectCardProps {
 
 export default function ProjectCard(props: ProjectCardProps) {
   return (
-    <Card className="lg:max-w-md w-[384px] h-[150px]">
+    <Card className="w-96 h-auto overflow-hidden drop-shadow">
       <div>
         <Image
           src={
             "https://th.bing.com/th/id/R.6dadd6a39af8ca284e56354ee099752e?rik=JXGf1AD1SgQOPg&pid=ImgRaw&r=0"
           }
           alt=""
-          width={384}
-          height={272}
-          className="aspect-square rounded-md object-cover overflow-hidden w-full md:w-auto max-h-fit"
+          width={1600}
+          height={1134}
+          className="aspect-square object-cover w-96 md:w-auto max-h-fit hover:scale-105 transition-transform duration-300 ease-in-out"
         />
       </div>
       <CardHeader>
         <CardTitle>Discord Bot</CardTitle>
-        <CardDescription>Python</CardDescription>
+        <CardDescription>
+        </CardDescription>
+        <div className="flex flex-wrap gap-1">
+          <Badge variant={"outline"}>JavaScript</Badge>
+          <Badge variant={"outline"}>Python</Badge>
+          <Badge variant={"outline"}>Discord</Badge>
+          <Badge variant={"outline"}>Python</Badge>
+          <Badge variant={"outline"}>Discord</Badge>
+        </div>
+      <Separator />
       </CardHeader>
-      <CardContent>Programming Discord Bot for using in server to help doing simple tasks, improving thinking and coding skills.</CardContent>
+      <CardContent>
+        Programming Discord Bot for using in server to help doing simple tasks,
+        improving thinking and coding skills.
+      </CardContent>
       <CardFooter className="flex justify-end">
-        <a href="Project URL">View Project</a>
+        <a href="https://github.com/Bthcorn/Cpp-Lab">View Project</a>
       </CardFooter>
     </Card>
   );
