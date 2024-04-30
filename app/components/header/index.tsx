32b108866c539baad40ded1105dd39e26ee8797d
Bthcorn <bowornthat.maize@gmail.com>
2024-04-30 11:03:30 +0000

    Update Nav

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -8,32 +8,38 @@ import {
   NavigationMenuTrigger,
 } from "../ui/navigation-menu";
 import { NavigationMenuItem } from "@radix-ui/react-navigation-menu";
-import { LinkIcon } from "lucide-react";
+import { LinkIcon, MenuIcon } from "lucide-react";
+import { motion } from "framer-motion";
 
 export default function Header() {
   return (
     <header className="z-[999] relative">
-      <div className="fixed top-6 left-1/2 -translate-x-1/2 w-96 h-14 rounded-lg border border-white border-opacity-40 bg-white bg bg-opacity-100 shadow-lg backdrop:blur-lg sm:w-[36rem] flex justify-center">
-        <div className="items-center flex">
-          <h1 className="text-2xl font-bold flex justify-start">Bthchth</h1>
-          <NavigationMenu className="">
-            <NavigationMenuList className="flex flex-1 gap-1">
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <NavigationMenuLink>Link</NavigationMenuLink>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Item Two</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <NavigationMenuLink>Link</NavigationMenuLink>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-          <LinkIcon className="w-6 h-6" />
-        </div>
+      <motion.div
+        initial={{ opacity: 0, y: -50, x: "-50%" }}
+        animate={{ opacity: 1, y: 0, x: "-50%" }}
+        transition={{ duration: 1 }}
+        className="fixed top-6 left-1/2 w-96 h-14 rounded-lg border border-white border-opacity-40 bg-white bg bg-opacity-100 shadow-lg backdrop:blur-lg sm:w-10/12">
+      </motion.div>
+      <div className="fixed top-6 left-1/2 -translate-x-1/2 w-96 h-14 rounded-lg border border-white border-opacity-40 bg-white bg bg-opacity-100 shadow-lg backdrop:blur-lg sm:w-10/12 flex flex-1 items-center justify-between">
+        <h1 className="text-xl font-bold px-2">Bthchth</h1>
+        <NavigationMenu className="">
+          <NavigationMenuList className="flex flex-1 gap-1">
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Item Two</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        {/* <LinkIcon className="w-6 h-6" /> */}
+        <MenuIcon className="w-6 h-6" />
       </div>
     </header>
   );
